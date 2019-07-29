86231775254350c28d91f016b297c3b560693d13
Bibek Saha <40909537+BibekSaha@users.noreply.github.com>
2019-07-29 23:50:55 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,24 @@
+const jokesDiv = document.querySelector(".joke");
+let i = 0;
+
+const grabJokes = async () => {
+    const jokeResponse = await fetch("https://geek-jokes.sameerkumar.website/api");
+
+    if (jokeResponse.status != 200) {
+        throw new Error();
+    }
+    const joke = await jokeResponse.json();
+
+    return joke;
+}
+
+const getJokes = () => {
+    grabJokes()
+    .then(joke => {
+        jokesDiv.innerHTML = joke;
+    });
+}
+
+getJokes();
+
+setInterval(getJokes, 10000);
